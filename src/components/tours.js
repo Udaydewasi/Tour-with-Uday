64dc4f0549b95582c268735f7e73eae779b833a8
Uday dewasi <143155466+Udaydewasi@users.noreply.github.com>
2024-07-08 19:35:36 +0530

    Add files via upload

---
+++ src/components/tours.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from './Card';
+
+function Tours({tours, removeTour}){
+    return (
+        <div className="container">
+            <div>
+                <h2 className="title">Plan with Uday</h2>
+            </div>
+            
+            <div className="cards">
+                {
+                    tours.map( (tours) => {
+                        return(
+                            <Card {...tours} removeTour = {removeTour}></Card>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Tours;
